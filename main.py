4b9fe3278f02f6e61365981f80c745e1dc00d804
nandika-k <nkarnik5@outlook.com>
2026-02-02 15:54:43 -0500

    top5rented query method added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 import mysql.connector
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
 
 print(os.getenv('MYSQL_DB_PASSWORD'))
 
@@ -15,16 +20,29 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
-cursor.execute("SHOW TABLES;")
-for table in cursor.fetchall():
-    print(table)
+@app.route('/api/top5rented', methods=['GET'])
+def get_top5rented():
+    #run sql query
+    cursor.execute("""
+        select f.film_id, f.title, count(f.film_id) as rental_count
+        from sakila.rental r
+        join sakila.inventory i on r.inventory_id = i.inventory_id 
+        join sakila.film f on i.film_id = f.film_id
+        group by f.film_id, f.title
+        order by rental_count desc limit 5;
+    """)
+    #store in results
+    results = cursor.fetchall()
 
-cursor.execute("""
-    SELECT film_id, title
-    FROM film
-    LIMIT 5;
-""")
-results = cursor.fetchall()
+    #process results into json format
+    films = []
+    for row in results:
+        films.append({
+            'film_id': row[0],
+            'title': row[1],
+            'rental_count': row[2]
+        })
+    return jsonify(films)
 
-for row in results:
-    print(row)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
